f3bbd726eb40ab753a0daa3943a18a686a45047a
Richard Knoll <richard@rknoll.at>
2019-06-03 10:06:29 +0100

    Always show debug logs.

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -10,7 +10,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const history = createBrowserHistory();
 
 const routerMiddleware = createRouterMiddleware(history);
-const loggerMiddleware = process.env.NODE_ENV === 'development' && createLoggerMiddleware();
+const loggerMiddleware = /*process.env.NODE_ENV === 'development' &&*/ createLoggerMiddleware();
 const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [
